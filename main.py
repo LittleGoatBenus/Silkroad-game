efdee43cf0a07e7b39d6e3451136bd230820ad6a
Luka <68807802+LittleGoatBenus@users.noreply.github.com>
2020-10-04 16:26:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import curses
 
-def menu(stdscr):
+import curses
+import sys
+from time import sleep
+
+
+
+def main(stdscr):
+    h, w = stdscr.getmaxyx()
     curses.curs_set(0)
     stdscr.clear()
     stdscr.nodelay(0)
@@ -13,8 +19,8 @@ def menu(stdscr):
 
         stdscr.addstr(10,20, """
 
-                                                  .-""...
-                                                 F   .-'
+                                                  .-""''._
+                                                 F   .-''
                                                 F   J
                                                I    I
                                                 L   `.
@@ -55,12 +61,17 @@ def menu(stdscr):
                    Y            Y           Y                                 Y
 
 
+
+
+
+                                         made by kebab ind.
         """)
 
         stdscr.addstr(32,53, "PLAY", graphics[0])
-        stdscr.addstr(33,52, "ABOUT", graphics[1])
+        stdscr.addstr(33,50, "ABOUT GAME", graphics[1])
         stdscr.addstr(34, 50, "CREATOR", graphics[2])
-        stdscr.addstr(35, 49, "EXIT", graphics[3])
+        stdscr.addstr(35, 50,  "INFO", graphics[3])
+        stdscr.addstr(36, 49, "EXIT", graphics[4])
 #
         stdscr.refresh()
 
@@ -77,36 +88,145 @@ def menu(stdscr):
     stdscr.clear()
 
 
-    if selection == 0:
 
+    if selection == 0:
+        curses.curs_set(0)
+        stdscr.clear()
+        stdscr.nodelay(0)
         selection2 = -1
         option2 = 0
 
-        while selection2 < 0:
-            graphics = [0]*5
-            graphics[option] = curses.A_REVERSE
+#TYPING EFFECT
+#words = "This is just a test :P"
+#for char in words:
+#    sleep(0.1)
+#    sys.stdout.write(char)
+#    sys.stdout.flush()
 
-            stdscr.addstr(5,5 ,"INCOMING MESSAGE")
-            stdscr.addstr(60,5, "ACCEPT", graphics[0])
-            stdscr.addstr(60,100, "ACCEPT", graphics[0])
+        try:
+            while selection2 < 0:
+                graphics = [0]*5
+                graphics[option2] = curses.A_REVERSE
 
-            stdscr.refresh()
+                stdscr.addstr(10,45, """
 
-            key = stdscr.getch()
 
-            if key == curses.KEY_UP:
-                option2 = (option-1)% 5
 
-            elif key == curses.KEY_DOWN:
-                option2 = (option+1)% 5
 
-            elif key == ord("\n"):
-                selection2 = option2
 
-        stdscr.clear()
+                """)
 
-        if selection == 0:
+                stdscr.addstr(55,10, "press enter to continue...", graphics[0])
+
+
+
+                stdscr.refresh()
+
+                key = stdscr.getch()
+
+                if key == curses.KEY_LEFT:
+                    option2 = (option2-1)% 5
+
+                elif key == curses.KEY_RIGHT:
+                    option2 = (option2+1)% 5
+
+                elif key == ord("\n"):
+                    selection2 = option2
+            stdscr.clear()
+
+
+        except curses.error:
             pass
 
+    elif selection == 1:
+        stdscr.clear()
+
+        try:
+
+                stdscr.addstr(0,0, """
+GAME INFO \n\n\n
+this game is about the hitman scam with the owner of the silkroad(ross ulbricht) aka dread pirate roberts
+
+The ultimate goal of the game is not to get caught being the owner of the silkroad aswell as not get scammed by
+
+several users. \n\n
+
+the game is mostly text based aswell as a choose your own adveture game.\n\n
+
+use the left and right arrow key to navigate between options and the enter key to select an option\n\n
+
+
+PRESS ANY KEY TO GO BACK...
+                 """)
+                stdscr.refresh()
+                stdscr.getch()
+                stdscr.clear()
+                curses.wrapper(main)
+
+
+        except curses.error:
+            pass
+
+    elif selection == 2:
+        stdscr.clear()
+
+        try:
+
+                stdscr.addstr(5,5, """
+CREATOR Information\n\n\n
+
+Kebab industries is a small orginisation I started with 2 friends, mostly small little game projects will be made\n\n
+
+
+Buy me a coffee(PayPal): legoytcreators@gmail.com\n\n
+
+PRESS ANY KEY TO GO BACK...
+                """)
+                stdscr.refresh()
+                stdscr.getch()
+                stdscr.clear()
+                curses.wrapper(main)
+
+        except curses.error:
+            pass
+
+    elif selection == 3:
+        stdscr.clear()
+
+        try:
+
+                stdscr.addstr(5,5, """
+BACKGROUNF INFORMATION\n\n\n
+
+
+MAIN CHARACTERS:
+
+Dread pirate roberts (YOU): the owner of the silkroad, real name Ross ulbricht
+
+Lucydrop : Silkroad vendor(seller), from canada
+
+Tony76 : Silkroad vendor(seller), from canada
+
+FriendlyChemist : Middle man between the hells angels and Lucydrop
+
+redandwhite : 'the hells angels' a motorbike gang which is experienced in drug selling on the streets
+
+\n\n\n
+
+Ross ulbricht was caught
+
+
+PRESS ANY KEY TO GO BACK...
+                """)
+                stdscr.refresh()
+                stdscr.getch()
+                stdscr.clear()
+                curses.wrapper(main)
+
+        except curses.error:
+            pass
+
+
+
 if (__name__ == "__main__"):
-    curses.wrapper(menu)
+    curses.wrapper(main)
